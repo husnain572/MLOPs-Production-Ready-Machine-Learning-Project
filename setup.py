99bfb3282df5a8929cc7a3a7727efdd0cb355123
Husnain <husnainmalikawan@gmail.com>
2025-07-20 10:52:55 -0700

    mongodb demo added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Husnan",
+    author_email="husnainmalikawan@gmail.com",
+    packages=find_packages()
+)
